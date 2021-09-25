8cd4f99df0bc99cc61a8213e15a3ffd60345c406
Sergey Kulikov <snowball@live.ru>
2021-09-25 17:07:06 +0300

    Add SFOutput node to pack subgraphs to shader functions

---
+++ Source/ShaderFlow/Private/MaterialNodes/SFOutput.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MaterialNodes/SFOutput.h"
+
+#include "MaterialCompiler.h"
+
+#define LOCTEXT_NAMESPACE "USFOutput"
+
+USFOutput::USFOutput(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+#if WITH_EDITORONLY_DATA
+	// Structure to hold one-time initialization
+	struct FConstructorStatics
+	{
+		FText NAME_ShaderFlow;
+		FConstructorStatics(const FString& DisplayName, const FString& FunctionName)
+			: NAME_ShaderFlow(LOCTEXT("ShaderFlow", "ShaderFlow"))
+		{
+		}
+	};
+	static FConstructorStatics ConstructorStatics(GetDisplayName(), GetFunctionName());
+
+	MenuCategories.Add(ConstructorStatics.NAME_ShaderFlow);
+
+	bCollapsed = true;
+#endif
+}
+
+#if WITH_EDITOR
+int32 USFOutput::Compile(class FMaterialCompiler* Compiler, int32 OutputIndex)
+{
+	if (Input.GetTracedInput().Expression)
+	{
+		return Compiler->CustomOutput(this, OutputIndex, Input.Compile(Compiler));
+	}
+	
+	return CompilerError(Compiler, TEXT("Input missing"));
+}
+
+uint32 USFOutput::GetInputType(int32 InputIndex)
+{
+	if (Input.GetTracedInput().Expression)
+		return Input.GetTracedInput().Expression->GetOutputType(Input.GetTracedInput().OutputIndex);
+	return MCT_Unknown;
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/ShaderFlow/Public/MaterialNodes/SFOutput.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Materials/MaterialExpressionCustomOutput.h"
+#include "SFOutput.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADERFLOW_API USFOutput : public UMaterialExpressionCustomOutput
+{
+	GENERATED_UCLASS_BODY()
+	
+	UPROPERTY(meta = (RequiredInput = "true"))
+	FExpressionInput Input;
+
+	virtual int32 GetNumOutputs() const override { return 1; }
+	virtual FString GetFunctionName() const override { return TEXT("GetSFOutput"); }
+	virtual FString GetDisplayName() const override { return TEXT("ShaderFlow Output"); }
+
+#if WITH_EDITOR
+	virtual int32 Compile(class FMaterialCompiler* Compiler, int32 OutputIndex) override;
+	
+	virtual void GetCaption(TArray<FString>& OutCaptions) const override { OutCaptions.Add(FString(TEXT("SFOutput"))); }
+	virtual uint32 GetInputType(int32 InputIndex) override;
+	virtual FExpressionInput* GetInput(int32 InputIndex) override { return &Input; }
+#endif
+};
